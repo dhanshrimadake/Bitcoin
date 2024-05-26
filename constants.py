4cc31417b72ea90622d558337f268c55a639bca4
dhanshrimadake <117923529+dhanshrimadake@users.noreply.github.com>
2024-05-26 21:39:36 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,29 @@
+consumer_key= 'G8hRivmnqj7b4gyvmRNyutbJg'
+consumer_secret= 'wentAqVmNKX9KRzUM93z8oQ1O9Il84NIf58bOKlqybWljdRwbz'
+
+access_token='1489279331998498816-QEMUDgWIKa7X6oRlcaVvHYhGcV5XSw'
+access_token_secret='O4pyGyr6QfRCr5IbpGd52okJRp2Qnt3PhAU9RdqSCLgWg'
+
+
+# consumer_key= '7x7w8Ti8GmMtuhvj7IXBuvZmP'
+# consumer_secret= 'kpFGcKiGxP9dJtlQzZ99p2OK9HM0IjA1I8n23N40VJyctOzvkG'
+#
+# access_token='1365544389498474497-97lzyZE9lroLNDzLhvW1dLS3MDKfV5'
+# access_token_secret='8mF9CrBt4SvmewPZaapgL7kkh2iJus824SvhBJOvDpXlw'
+
+num_of_tweets = int(300)
+
+
+
+# G8hRivmnqj7b4gyvmRNyutbJg
+#
+# wentAqVmNKX9KRzUM93z8oQ1O9Il84NIf58bOKlqybWljdRwbz
+#
+# AAAAAAAAAAAAAAAAAAAAACKiYwEAAAAAn2fGqNp%2FxuYLJi%2BqrsKoilsvEns%3DE7jJvJlbGKM1rIMmQ8JvKmEiLoM9TWPbpcSqMOdTS4uQ42i1LY
+#
+#
+#
+#
+#
+#
+# O4pyGyr6QfRCr5IbpGd52okJRp2Qnt3PhAU9RdqSCLgWg
